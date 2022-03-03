362ec8e16a23c689d6573be50e37cc04fc9d9c6b
Jakku <j97k107u45k117n@gmail.com>
2022-03-03 15:59:26 +0000

    MySQL Database connection support added.

---
+++ backend/database/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+
+const db = mysql.createPool({
+    host: "localhost",
+    port: "3360",
+    database: "db",
+    user: "user",
+    password: "password"
+});
+
+db.getConnection((err, connection) => {
+    if(!connection || err){
+        console.log("[DB -> getConnection()] FATAL ERROR.");
+        console.log(err);
+        return -1;
+    }else{
+        console.log("[DB -> getConnection()] DATABASE CONNECTED.");
+    }
+});
+
+db.query = promisify(db.query);
+
+module.exports = db;
