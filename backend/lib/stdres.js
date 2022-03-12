5fcac8d2a2c6a9f4632608ccf536d1aec02d6611
Jakku <j97k107u45k117n@gmail.com>
2022-03-12 00:06:42 +0000

    STD RESPONSES file added.

---
+++ backend/lib/stdres.js
@@ -0,0 +1,27 @@
+const stdres = {};
+
+stdres.ok = JSON.stringify({
+    http_code: 200,
+    status: 0,
+    msg: "OK."
+});
+
+stdres.auth_error = JSON.stringify({
+    http_code: 401,
+    status: 2,
+    msg: "AUTH ERROR."
+});
+
+stdres.fatal_error = JSON.stringify({
+    http_code: 400,
+    status: -1,
+    msg: "FATAL ERROR."
+});
+
+stdres.not_found = JSON.stringify({
+    http_code: 404,
+    status: 1,
+    msg: "OK"
+});
+
+module.exports = stdres;
