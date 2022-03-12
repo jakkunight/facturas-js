1982ab27ce972a7f708c2fa32888b47a051bd0ef
Jakku <j97k107u45k117n@gmail.com>
2022-03-12 01:53:36 +0000

    Added debug log for cliet's requests.

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 
 const app = express();
 
-app.set('port', 3000);
+app.set('port', process.env.PORT | 3000);
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(async (req, res, next) => {
+    await console.log(`[${req.method.toUpperCase()}] ${req.protocol}://${req.hostname}:${app.get('port')}${req.url}`);
+    await console.log(`[CLIENT METADATA]`);
+    await console.log(`[IP] ${req.ip}`);
+    await console.log(`[USER] ${req.user}`);
+    await console.log(`[FILES] ${req.files}`);
+    await console.log(`[BODY] ${req.body}`);
+    await console.log(`[PARAMS] ${req.params}`);
+    await console.log(`[COOKIES] ${req.cookies}`);
+    next();
+});
 
 app.listen(app.get('port'), () => {
     console.log("[APP -> listen()] SERVER ON PORT", app.get('port'));
