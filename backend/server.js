f51c2d72398919fe8b206102c9e614eb24d7b60e
Jakku <j97k107u45k117n@gmail.com>
2022-03-14 22:18:18 +0000

    Added 'express-fileupload' middleware. Added CORS middleware. Testing routes.

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const fileupload = require('express-fileupload');
+const cors = require('cors');
 const path = require('path');
+const products = require('./routes/api/products');
 
 const app = express();
 
 app.set('port', process.env.PORT | 3000);
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+}));
+app.use(fileupload());
 
 app.use(async (req, res, next) => {
-    await console.log(`[${req.method.toUpperCase()}] ${req.protocol}://${req.hostname}:${app.get('port')}${req.url}`);
-    await console.log(`[CLIENT METADATA]`);
-    await console.log(`[IP] ${req.ip}`);
-    await console.log(`[USER] ${req.user}`);
-    await console.log(`[FILES] ${req.files}`);
-    await console.log(`[BODY] ${req.body}`);
-    await console.log(`[PARAMS] ${req.params}`);
-    await console.log(`[COOKIES] ${req.cookies}`);
+    console.log(`[${req.method.toUpperCase()}] ${req.protocol}://${req.hostname}:${app.get('port')}${req.url}`);
+    console.log(`[CLIENT METADATA]`);
+    console.log(`[IP] ${req.ip}`);
+    console.log(`[USER] ${req.user}`);
+    console.log(`[FILES] ${req.files}`);
+    console.log(`[BODY] ${req.body}`);
+    console.log(`[PARAMS] ${req.params}`);
+    console.log(`[COOKIES] ${req.cookies}`);
     next();
 });
+
+app.use(products);
 
 app.listen(app.get('port'), () => {
     console.log("[APP -> listen()] SERVER ON PORT", app.get('port'));
